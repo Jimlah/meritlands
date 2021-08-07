5d4349e6b3e570004b02799f30c5dca78df2ac58
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-08-07 11:58:43 +0100

    added  the logout method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -41,4 +41,11 @@ class AuthController extends Controller
 
         return redirect()->back()->with('error', 'Invalid email or password');
     }
+
+    public function logout()
+    {
+        Auth::logout();
+
+        return redirect()->route('login.view');
+    }
 }
