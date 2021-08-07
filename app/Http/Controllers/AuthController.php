d6919e8b6058f1a741aa0ad51be85d810a1ec84f
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-08-07 09:06:00 +0100

    Created Login Method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginFormRequest;
 use App\Http\Requests\RegisterFormRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -18,7 +20,7 @@ class AuthController extends Controller
         $user->password = bcrypt($request->password);
         $user->save();
 
-        // return response()->view()
+        return redirect(route('login.view'))->with('message', 'You have successfully registered!');
     }
 
     public function registerView()
@@ -29,5 +31,14 @@ class AuthController extends Controller
     public function loginView()
     {
         return response()->view('auth.login');
+    }
+
+    public function login(LoginFormRequest $request)
+    {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            return redirect()->intended('/');
+        }
+
+        return redirect()->back()->with('error', 'Invalid email or password');
     }
 }
