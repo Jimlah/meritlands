c0cf58efa597b93c649154dfb45376973350b82c
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-08-07 08:35:37 +0100

    Created Register method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterFormRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function register(Request $request)
+    public function register(RegisterFormRequest $request)
     {
-        
+        $user = new User();
+
+        $user->firstname = $request->firstname;
+        $user->lastname = $request->lastname;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+
+        // return response()->view()
     }
 }
