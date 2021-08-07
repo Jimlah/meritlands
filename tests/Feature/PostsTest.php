a66f8622d01b93024d3dfd7d436720a2c7f07ae5
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-08-07 17:32:16 +0100

    Create test for post creation

--- tests/Feature/PostsTest.php
+++ tests/Feature/PostsTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use App\Models\Post;
 use App\Models\User;
+use Faker\Factory;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -33,5 +34,35 @@ class PostsTest extends TestCase
         Post::factory(5)->create();
         $response = $this->get('/posts');
         $response->assertStatus(500);
+    }
+
+    public function test_user_can_create_post()
+    {
+        Artisan::call('migrate');
+        User::factory()->create();
+
+        $faker = Factory::create();
+        $response = $this->actingAs(User::find(1))->post('/posts', [
+            'title' => $faker->text(10),
+            'content' => $faker->paragraph(4),
+            'category' => $faker->randomElement(['News', 'Events', 'Articles', 'Poetry']),
+        ]);
+
+        $response
+            ->assertStatus(302)
+            ->assertRedirect('/posts')
+            ->assertSessionHas('success');
+    }
+
+    public function test_user_can_not_create_post_without_title()
+    {
+        Artisan::call('migrate');
+        User::factory()->create();
+        $response = $this->actingAs(User::find(1))->post('/posts', [
+            'content' => 'content',
+            'category' => 'category',
+        ]);
+        $response->assertStatus(302);
+        $response->assertSessionHas('errors');
     }
 }
