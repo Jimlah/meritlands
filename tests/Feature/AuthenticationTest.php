30475784b11103a1d3576d6a04877197d2e2ae05
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-08-07 11:32:31 +0100

    added validation testing to Authentication

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -2,12 +2,23 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
+use Faker\Factory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Artisan;
+use phpDocumentor\Reflection\Types\Parent_;
 use Tests\TestCase;
 
 class Authentication extends TestCase
 {
+
+    protected function setUp(): void
+    {
+        Parent::setUp();
+    }
+
     /**
      * A basic feature test example.
      *
@@ -27,5 +38,76 @@ class Authentication extends TestCase
 
         $response->assertStatus(200);
         $response->assertViewIs('auth.login');
+    }
+
+    public function test_user_can_register()
+    {
+
+        Artisan::call('migrate');
+
+        $faker = Factory::create();
+
+        $response = $this->post('/register', [
+            'firstname' => $faker->firstName,
+            'lastname' => $faker->lastName,
+            'email' => $faker->safeEmail(),
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $response->assertRedirect(route('login.view'));
+        $response->assertSessionHas('message');
+    }
+
+    public function test_user_can_login()
+    {
+        Artisan::call('migrate');
+
+        $user = User::factory()->create();
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+
+        $response->assertRedirect(route('home'));
+    }
+
+    /**
+     * @dataProvider registerDataProvider
+     *
+     * */
+    public function test_user_can_not_register_invalid_details($invalidData, $invalidFields)
+    {
+        // Artisan::call('migrate');
+
+        $response = $this->post('/register', $invalidData);
+
+        $response->assertSessionHasErrors($invalidFields)->assertStatus(302);
+    }
+
+    public function registerDataProvider()
+    {
+        $faker = Factory::create();
+        // $user = User::factory()->create();
+
+        return [
+            [
+                ['firstname' => $faker->firstName],
+                ['lastname', 'email', 'password', 'password_confirmation'],
+            ],
+            [
+                ['firstname' => $faker->firstName, 'lastname' => $faker->lastName, 'email' => $faker->email, 'password' => 'password', 'password_confirmation' => 'password1'],
+                ['password_confirmation'],
+            ],
+            [
+                ['firstname' => $faker->firstName, 'lastname' => $faker->lastName, 'email' => "aaaa", 'password' => 'password', 'password_confirmation' => 'password'],
+                ['email'],
+            ],
+            // [
+            //     ['firstname' => $faker->firstName, 'lastname' => $faker->lastName, 'email' => $user->email, 'password' => 'password', 'password_confirmation' => 'password'],
+            //     ['email'],
+            // ],
+        ];
     }
 }
