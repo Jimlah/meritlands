ef52f4179c2a59e9fac0a1c7067d9f5592095cb9
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-08-10 21:43:39 +0100

    Updated the blog panel

--- resources/views/components/blog-panel.blade.php
+++ resources/views/components/blog-panel.blade.php
@@ -3,9 +3,9 @@
     <div class="w-full h-96 bg-cover bg-center bg-no-repeat rounded-lg"
         style="background-image: url('https://dyl80ryjxr1ke.cloudfront.net/external_assets/hero_examples/hair_beach_v391182663/original.jpeg')">
     </div>
-    <div class="w-full flex flex-col items-start space-x-1 h-48 overflow-y-hidden text-gray-900">
-        <h3 class="font-bold text-2xl tracking-wider break-words dark:text-gray-200">Title</h3>
-        <p class="text-normal tracking-wider leading-5 break-words dark:text-gray-200 text-opacity-75">
+    <div class="w-full flex flex-col items-start space-y-3 h-48 overflow-y-hidden text-gray-900">
+        <h3 class="font-bold text-3xl tracking-wider break-words dark:text-gray-200">Title</h3>
+        <p class="text-lg tracking-wide leading-5 text-justify break-words dark:text-gray-200 text-opacity-75">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error earum voluptatibus ab architecto ipsam est
             aliquid, sequi consectetur quasi reprehenderit expedita animi aut similique in temporibus accusamus
             laudantium esse eveniet!
