2e1c09e47ce89a93f25499abff127c58b40b5bd1
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-08-10 10:48:13 +0100

    Added dark Mode styling

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,8 +1,8 @@
-<nav class="px-5 flex items-center justify-between w-full static md:px-10" x-data="dropdown">
-    <span class="font-bold text-4xl py-6 text-gray-700"> MeritLand </span>
+<nav class="px-5 flex items-center justify-between w-full static md:px-10 text-gray-700 dark:text-gray-200" x-data="dropdown">
+    <span class="font-bold text-4xl py-6 "> MeritLand </span>
     <div class="flex items-center space-x-2 static" x-data="darkmode">
         <button x-on:click="change">
-            <svg class="w-8 h-8 text-gray-700" :class="{'hidden': dark == false}" fill="none" stroke="currentColor"
+            <svg class="w-8 h-8" :class="{'hidden': dark == false}" fill="none" stroke="currentColor"
                 viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z">
@@ -14,8 +14,8 @@
                     d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"></path>
             </svg>
         </button>
-        <button x-on:click="toggle">
-            <svg class="w-10 h-10 text-gray-700" :class="{'hidden': open == false}" fill="none" stroke="currentColor"
+        <button x-on:click="toggle" class="">
+            <svg class="w-10 h-10" :class="{'hidden': open == false}" fill="none" stroke="currentColor"
                 viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16">
                 </path>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,18 +21,18 @@
     </style>
 </head>
 
-<body class="antialiased">
+<body class="antialiased dark:bg-gray-900">
     <x-navbar></x-navbar>
-    <header class="grid grid-cols-1 gap-5 md:grid-cols-2">
+    <header class="grid grid-cols-1 gap-5 md:grid-cols-2 text-gray-700 dark:text-gray-200">
         <div class="px-5 pt-10 flex items-center justify-start md:px-10">
-            <div class="text-gray-700 text-left flex flex-col items-start space-y-5">
+            <div class="text-left flex flex-col items-start space-y-5">
                 <h1 class="font-bold capitalize text-2xl leading-7 tracking-wide">dolor repellat sit temporibus odio ad
                     necessitatibus aperiam accusantium quisquam optio</h1>
-                <p class="text-xl text-gray-500 text-opacity-95">Lorem ipsum dolor sit amet consectetur adipisicing
+                <p class="text-xl text-gray-500 text-opacity-50 dark:text-opacity-75">Lorem ipsum dolor sit amet consectetur adipisicing
                     elit. Nihil, reiciendis quibusdam eos possimus perferendis aspernatur tempore nostrum laudantium
                     obcaecati?</p>
                 <button
-                    class="bg-gray-900 text-gray-100 px-5 font-bold text-2xl py-2 rounded-full capitalize">subscribe</button>
+                    class="bg-gray-900 dark:bg-gray-700 hover:bg-opacity-50 text-gray-100 px-5 font-bold text-2xl py-2 rounded-full capitalize">subscribe</button>
             </div>
         </div>
 
@@ -47,7 +47,7 @@
     <section class="px-5 py-10 grid-cols-1 gap-5 grid md:px-10 md:py-20 md:justify-items-center">
         <div class="flex flex-col justify-center items-left space-y-4 md:items-center">
             <h2 class="tracking-widest text-sm uppercase text-gray-400">My Newsletter</h2>
-            <p class="text-2xl font-semibold leading-7 tracking-wide text-left md:w-1/2 md:text-center">Lorem ipsum
+            <p class="text-2xl dark:text-gray-200 font-semibold leading-7 tracking-wide text-left md:w-1/2 md:text-center">Lorem ipsum
                 dolor sit, amet consectetur adipisicing elit. Unde hic consequuntur, sapiente molestiae perspiciatiss
             </p>
         </div>
@@ -56,15 +56,56 @@
             <div class="h-36 w-36 border rounded-full bg-cover bg-center bg-no-repeat"
                 style="background-image: url('https://images.unsplash.com/photo-1578489758854-f134a358f08b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzZ8fHBlcnNvbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80')">
             </div>
-            <article class="text-gray-400 leading-5 text-sm tracking-wider max-w-md">
+            <article class="text-gray-700 leading-5 text-base tracking-wider max-w-md">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, vitae! Quis ullam quas odit vel
                     facilis dolor quam ex ducimus expedita, quae sunt obcaecati voluptatibus, incidunt atque voluptates
                     corporis labore!</p>
                 <p class="mt-3">
-                    <span class="font-bold text-xl text-gray-900">Tema Gray</span><br />
+                    <span class="font-bold text-xl">Tema Gray</span><br />
                     <span class="text-xs">Leading Developer</span>
                 </p>
             </article>
+        </div>
+    </section>
+    <section class="md:px-10 md:py-20 flex items-center justify-center">
+        <div class="bg-gray-300  bg-opacity-75 grid grid-cols-1 md:grid-cols-2 px-5 py-10 rounded-lg w-full gap-5">
+            <div class="flex flex-col items-start space-y-4">
+                <span>
+                    <svg class="w-10 h-10 text-gray-100" fill="#000" stroke="currentColor" viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M12 8v13m0-13V6a2 2 0 112 2h-2zm0 0V5.5A2.5 2.5 0 109.5 8H12zm-7 4h14M5 12a2 2 0 110-4h14a2 2 0 110 4M5 12v7a2 2 0 002 2h10a2 2 0 002-2v-7">
+                        </path>
+                    </svg>
+                </span>
+                <p class="font-bold leading-6 tracking-wide text-2xl">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo recusandae ducimus, incidunt illum
+                    veniam quam aliquam.
+                </p>
+                <p class="leading-6 tracking-wide text-gray-500 text-sxl">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo recusandae ducimus, incidunt illum
+                    veniam quam aliquam.
+                </p>
+            </div>
+            <div>
+                <form action="" method="POST" class="flex flex-col space-y-5 items-start">
+                    <input type="text" name="name" placeholder="firstname"
+                        class="w-full rounded-full px-5 py-3 uppercase tracking-widest" />
+                    <input type="email" name="email" placeholder="Email Address"
+                        class="w-full rounded-full px-5 py-3 uppercase tracking-widest" />
+                    <button type="submit"
+                        class="bg-gray-900 hover:bg-opacity-70 text-gray-100 flex space-x-2 px-5 py-3 rounded-full">
+                        <span class="w-full rounded-full uppercase tracking-widest">Join Now</span>
+                        <span>
+                            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M17 8l4 4m0 0l-4 4m4-4H3"></path>
+                            </svg>
+                        </span>
+                    </button>
+                </form>
+            </div>
         </div>
     </section>
     <footer>
