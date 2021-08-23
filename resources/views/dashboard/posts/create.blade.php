312627150ad32b1e896f50d42e0cce017a9b3d92
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-08-23 15:49:23 +0100

    Modified the create view

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -2,18 +2,21 @@
 
 @section('content')
     <x-section>
-        <form action="{{ route('posts.store') }}" method="POST" enctype="multipart/form-data" class="w-full flex flex-col justify-start space-y-5">
+        <form action="{{ route('posts.store') }}" method="POST" enctype="multipart/form-data"
+            class="w-full flex flex-col justify-start space-y-5">
             @csrf
-            <x-input type="text" name="title" label="Title" placeholder="Enter your title" :error="$errors"></x-input>
+            <x-input type="text" name="title" label="Title" placeholder="Enter your title" :error="$errors"
+                value="{{ old('title') }}"></x-input>
             <div>
                 <label for="content"
                     class="text-normal font-bold text-gray-900 text-opacity-50 dark:text-gray-500">Content</label>
                 <textarea name="content" id="content" cols="30" rows="10"
-                    class="w-full focus:outline-none border px-3 py-2 rounded-md text-gray-900 bg-transparent dark:text-gray-50 hover:border-gray-900 dark:hover:border-gray-50 ckeditor"></textarea>
+                    class="w-full focus:outline-none border px-3 py-2 rounded-md text-gray-900 bg-transparent dark:text-gray-50 hover:border-gray-900 dark:hover:border-gray-50 ckeditor">{{ old('content') }}</textarea>
             </div>
-            <x-input type="file" name="image" label="Image" :error="$errors"></x-input>
+            <x-input type="file" name="image" label="Image" :error="$errors" value="{{ old('image') }}"></x-input>
 
-            <x-input type="text" name="category" label="Category" placeholder="Enter the category" :error="$errors">
+            <x-input type="text" name="category" label="Category" placeholder="Enter the category" :error="$errors"
+                value="{{ old('category') }}">
             </x-input>
             <div class="flex items-center justify-start space-x-3">
                 <button type="submit" name="save"
@@ -23,14 +26,11 @@
             </div>
         </form>
     </x-section>
-<script src="//cdn.ckeditor.com/4.14.1/standard/ckeditor.js"></script>
+    <script src="//cdn.ckeditor.com/4.14.1/standard/ckeditor.js"></script>
     <script type="text/javascript">
-        $(document).ready(function () {
-            $('.ckeditor').ckeditor();
+        CKEDITOR.replace('content', {
+            filebrowserUploadUrl: "{{ route('ckeditor.image-upload', ['_token' => csrf_token()]) }}",
+            filebrowserUploadMethod: 'form'
         });
-         CKEDITOR.replace('wysiwyg-editor', {
-        filebrowserUploadUrl: "{{route('ckeditor.image-upload', ['_token' => csrf_token() ])}}",
-        filebrowserUploadMethod: 'form'
-    });
     </script>
 @endsection()
