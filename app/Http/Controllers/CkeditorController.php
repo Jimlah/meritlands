a2de2dd7f43d195daae51f1d9ddeda85d13846ab
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-08-23 15:48:24 +0100

    created controller for Ckeditor

--- app/Http/Controllers/CkeditorController.php
+++ app/Http/Controllers/CkeditorController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\ImageUploadToCloudinary;
 use Illuminate\Http\Request;
 
 class CkeditorController extends Controller
 {
     public function imageUpload(Request $request)
     {
+       if ($request->hasFile('upload')) {
 
+          $imageUrl = $request->file('upload')->storeOnCloudinary()->getSecurePath();
+          $CKEditorFuncNum = $request->input('CKEditorFuncNum');
+          $msg = 'Image uploaded successfully';
+          $response = "<script>window.parent.CKEDITOR.tools.callFunction($CKEditorFuncNum, '$imageUrl', '$msg')</script>";
+
+@header('Content-type: text/html; charset=utf-8');
+          echo $response;
+       }
     }
 }
